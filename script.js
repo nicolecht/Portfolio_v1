12cfa2a21998a1d03a95eaeb4e53b95bc6f08f73
nicolecht <nicolecht@hotmail.com>
2022-08-13 19:06:24 +0800

    navbar and home section

---
+++ script.js
@@ -0,0 +1,11 @@
+// Show navbar on scroll 
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        }
+        else {
+            $('.navbar').removeClass("sticky");
+        }
+    })
+});

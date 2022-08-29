600ffb9b83b182dd3047d4446382939524ea51bf
nicolecht <nicolecht@hotmail.com>
2022-08-29 11:23:30 +0800

    added OwlCarousel

--- script.js
+++ script.js
@@ -14,4 +14,29 @@ $(document).ready(function() {
         $('.navbar .menu').toggleClass("active");
         //$('.menu-btn i').toggleClass("active");
     });
+
+    // Owl Carousel
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        aitoplayHoverPause: true,
+        responsive: {
+            // breakpoint from 0 up
+            0:{
+                items: 1,
+                nav: false
+            },
+            // breakpoint from 600 up
+            600:{
+                items: 2,
+                nav: false
+            },
+            // breakpoint from 960 up
+            960:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
 });

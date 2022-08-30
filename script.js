7fd5cbaf533042d0f585aebeb5b55b48956bd5f4
nicolecht <nicolecht@hotmail.com>
2022-08-30 17:48:04 +0800

    scroll and animations completed

--- script.js
+++ script.js
@@ -8,12 +8,38 @@ $(document).ready(function() {
         else {
             $('.navbar').removeClass("sticky");
         }
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        }
+        else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function() {
+        $('html').animate({scrollTop: 0}, 50);
     });
 
     // Toggle menu/navbar for mobile
     $('.menu-btn').click(function() {
         $('.navbar .menu').toggleClass("active");
         //$('.menu-btn i').toggleClass("active");
+    });
+
+    // Typing animation script
+    var typed = new Typed(".typing", {
+        strings: ["developer", "42KL cadet", "puzzle-addict"],
+        typeSpeed: 60,
+        backSpeed: 30,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["developer", "42KL cadet", "puzzle-addict"],
+        typeSpeed: 60,
+        backSpeed: 30,
+        loop: true
     });
 
     // Owl Carousel

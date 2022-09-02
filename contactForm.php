2530e422b3b9d9a1a3b09700338ac9a8b2fb9be9
nicolecht <nicolecht@hotmail.com>
2022-09-02 10:56:56 +0800

    php contact form added

---
+++ contactForm.php
@@ -0,0 +1,17 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $mailFrom = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $mailTo = "nicolecht@hotmail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have received an email from ".$name.".\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.php?mailsend");
+}
+
+?>

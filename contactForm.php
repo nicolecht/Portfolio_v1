39ff52fb94aa4de7a9c2058f9dc524541ef0353c
nicolecht <nicolecht@hotmail.com>
2022-09-02 12:01:54 +0800

    updated CV

--- contactForm.php
+++ contactForm.php
@@ -11,7 +11,7 @@ if (isset($_POST['submit'])) {
     $txt = "You have received an email from ".$name.".\n\n".$message;
 
     mail($mailTo, $subject, $txt, $headers);
-    header("Location: index.php?mailsend");
+    header("Location: index.html#contact");
 }
 
 ?>
